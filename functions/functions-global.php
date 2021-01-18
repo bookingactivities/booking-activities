46c98651e68bf59830d7e78091919b554d5c298a
yoan_cutillas <yoan.cutillas@gmail.com>
2021-01-18 16:17:15 +0100

    * Update required addons version to there beta versions

--- functions/functions-global.php
+++ functions/functions-global.php
@@ -648,7 +648,7 @@ function bookacti_get_add_ons_data( $prefix = '', $exclude = array( 'balau' ) ) 
 			'plugin_name'	=> 'ba-notification-pack', 
 			'end_of_life'	=> '', 
 			'download_id'	=> 1393,
-			'min_version'	=> '1.2.4'
+			'min_version'	=> '1.2.4-beta1'
 		),
 		'bapap' => array( 
 			'title'			=> 'Prices and Credits', 
@@ -656,7 +656,7 @@ function bookacti_get_add_ons_data( $prefix = '', $exclude = array( 'balau' ) ) 
 			'plugin_name'	=> 'ba-prices-and-credits', 
 			'end_of_life'	=> '', 
 			'download_id'	=> 438,
-			'min_version'	=> '1.4.16'
+			'min_version'	=> '1.4.16-beta1'
 		),
 		'baaf' => array( 
 			'title'			=> 'Advanced Forms', 
@@ -664,7 +664,7 @@ function bookacti_get_add_ons_data( $prefix = '', $exclude = array( 'balau' ) ) 
 			'plugin_name'	=> 'ba-advanced-forms', 
 			'end_of_life'	=> '', 
 			'download_id'	=> 2705,
-			'min_version'	=> '1.2.13'
+			'min_version'	=> '1.2.13-beta1'
 		),
 		'baofc'	=> array( 
 			'title'			=> 'Order for Customers', 
@@ -672,7 +672,7 @@ function bookacti_get_add_ons_data( $prefix = '', $exclude = array( 'balau' ) ) 
 			'plugin_name'	=> 'ba-order-for-customers', 
 			'end_of_life'	=> '', 
 			'download_id'	=> 436,
-			'min_version'	=> '1.2.14'
+			'min_version'	=> '1.2.14-beta1'
 		),
 		'balau' => array( 
 			'title'			=> 'Licenses & Updates', 
